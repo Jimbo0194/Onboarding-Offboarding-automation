4b133f39742b5d7aacc8857a8a4873b4ff881800
Jimbo0194 <43893621+Jimbo0194@users.noreply.github.com>
2020-11-10 15:04:33 -0600

    Update Create_users_and_copy_access.ps1

--- Create_users_and_copy_access.ps1
+++ Create_users_and_copy_access.ps1
@@ -6,10 +6,11 @@ $new_user_logon = Read-Host -Prompt 'Enter user´s logon (SamAccountName)'
 $main_email_new_user = Read-Host -Prompt 'Enter user´s main email'
 $user_manager = Read-Host -Prompt 'Enter the manager´s name of the user (add " " at the beggining and end)'
 $user_job_title = Read-Host -Prompt 'Enter the user´s job title'
+$user_company = Read-Host -Prompt 'Enter User´s Company:'
 
 $user_department= Read-Host -Prompt 'Enter user´s department'
 
-New-ADUser -Name $name_new_user -GivenName $Firstname -Surname $surname -SamAccountName $new_user_logon -UserPrincipalName $main_email_new_user -Path "OU=Users,OU=CORP,DC=Centric,DC=US" -AccountPassword(Read-Host -AsSecureString "Type Password for User") -ChangePasswordAtLogon $true -Enabled $true -Manager $user_manager -Email $main_email_new_user -Department $user_department -Title $user_job_title
+New-ADUser -Name $name_new_user -GivenName $Firstname -company $user_company -Surname $surname -SamAccountName $new_user_logon -UserPrincipalName $main_email_new_user -Path "OU=Users,OU=CORP,DC=Centric,DC=US" -AccountPassword(Read-Host -AsSecureString "Type Password for User") -ChangePasswordAtLogon $true -Enabled $true -Manager $user_manager -Email $main_email_new_user -Department $user_department -Title $user_job_title
 $user_expires = Read-Host -Prompt 'Is the user temporal? (y/n)'
 
 # Select if the user expires or not
