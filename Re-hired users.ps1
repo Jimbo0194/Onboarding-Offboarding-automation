61620045691013f3d3c074a439b1008ccfedc088
Jimbo0194 <43893621+Jimbo0194@users.noreply.github.com>
2020-11-23 16:34:43 -0600

    Update Re-hired users.ps1

--- Re-hired users.ps1
+++ Re-hired users.ps1
@@ -3,12 +3,11 @@
 #move disabled users to Users OU
 $user = Read-Host -Prompt 'Enter username'
 $user_expires = Read-Host -Prompt "Is the user temporal? (y/n)"
-$user_location = get-aduser $user | Select-Object distinguishedname
-$user_location = """$user_location"""
-move-adobject -identity $user_location "OU=Users,OU=Corporate,DC=domain,DC=com"
+$user_location = get-aduser $user | Select-Object distinguishedname -ExpandProperty distinguishedname -First 1
+move-adobject -identity "$user_location" "OU=Users,OU=CORP,DC=domain,DC=com"
 Enable-ADAccount $user
 # Select if the user expires or not
-if ($user_expires -eq 'y') {
+if ($user_expires -like 'y') {
 
     Get-ADUser $user | Set-ADAccountExpiration -TimeSpan 90.0:0
     Write-Host 'User will expire in 90 days'
@@ -20,8 +19,7 @@ if ($user_expires -eq 'y') {
 }
 # Copy all memberships from existing user to new user
 $copy = Read-host "Enter user to copy from"
-$Sam  = Read-host " Enter user to copy to"
-Get-ADUser -Identity $copy -Properties memberof | Select-Object -ExpandProperty memberof |  Add-ADGroupMember -Members $Sam
+Get-ADUser -Identity $copy -Properties memberof | Select-Object -ExpandProperty memberof |  Add-ADGroupMember -Members $user
 
 # Run a Get-ADUser and a net user to check the account and its setup
 Get-ADUser $user -Properties office, manager, department, title
